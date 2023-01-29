56b108edd583551a8214a386fda115f46fe9d6c4
josesanmiguel <sanmiguel_jose@outlook.com>
2023-01-29 21:35:50 +1300

    Photos, classes, and some Strings are ready.

---
+++ app/src/main/java/com/practice/artspace/Caballeros.kt
@@ -0,0 +1,11 @@
+package com.practice.artspace
+
+data class Caballeros(var nombreReal: String, var armadura: String, var poderEspecial: String){
+}
+
+val pegasus = Caballeros("Seiya","Armadura de Pegasus", "Meteoro de pegasus")
+val cisne = Caballeros("Hyoga", "Armadura del Cisne", "Polvo de diamantes")
+val fenix = Caballeros("Ikki", "Armadura del Fenix", "Ave fenix")
+val dragon = Caballeros("Shiryu", "Armadura del Dragon", "La furia del dragon")
+val andromeda = Caballeros("Shawn", "Armadura de Andromeda", "Cadena de Andromeda")
+
--- app/src/main/java/com/practice/artspace/MainActivity.kt
+++ app/src/main/java/com/practice/artspace/MainActivity.kt
@@ -3,12 +3,18 @@ package com.practice.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.practice.artspace.ui.theme.ArtSpaceTheme
 
@@ -22,22 +28,49 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ArtSpace()
                 }
             }
         }
     }
 }
 
+//Joint whole functions to make the application
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+@Preview(showSystemUi = true)
+fun ArtSpace() {
+    Column {
+        ImageDisplay()
+        TextDescription()
+        ButtonsNextAndPrevious()
+    }
+
 }
 
-@Preview(showBackground = true)
+//there are three functions (show Image, about Image, buttons next and previous)
 @Composable
-fun DefaultPreview() {
-    ArtSpaceTheme {
-        Greeting("Android")
+fun ImageDisplay() {
+    Image(
+        painter = painterResource(id = R.drawable.fenix),
+        contentDescription = stringResource(id = R.string.fenix)
+    )
+}
+
+@Composable
+fun TextDescription(){
+    Text(text = fenix.armadura)
+    Text(text = fenix.nombreReal)
+    Text(text = fenix.poderEspecial)
+}
+
+@Composable
+fun ButtonsNextAndPrevious(){
+    Row {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Previos")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Next")
+        }
     }
 }
